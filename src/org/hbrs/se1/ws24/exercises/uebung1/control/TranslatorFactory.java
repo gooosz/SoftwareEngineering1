12ac711f15135b880b53bd6e13d844ac938262ee
Len <lmadler72@eclipso.de>
2024-10-09 11:25:11 +0200

    Notizen und Verbesserungen zu Übung1

--- src/org/hbrs/se1/ws24/exercises/uebung1/control/TranslatorFactory.java
+++ src/org/hbrs/se1/ws24/exercises/uebung1/control/TranslatorFactory.java
@@ -4,9 +4,15 @@ import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 
 public class TranslatorFactory {
-    /* Factory erzeugt verschiedene Translators */
+    /* Factory erzeugt verschiedene Translators
+     * sinnvoll wenn Caller nur das Objekt haben will, aber wie es
+     * zusammengebaut wird ist nicht responsibility vom Caller (single responsibility)
+     *
+     * Fabrik soll Translator bei Methoden zurückgeben, damit
+     * Client nur abhängig hat zu Translator und nicht zusätzlich zu GermanTranslator
+    */
 
-    public static GermanTranslator createGermanTranslator() {
+    public static Translator createGermanTranslator() {
         GermanTranslator t = new GermanTranslator();
         // for setting the date of creation of translator in format "MMMM/yyyy"
         // with month string representation of the month abbreviated to 3 letters
