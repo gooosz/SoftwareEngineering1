dc1efc14ff94bebc6d7040f9fe5728abfba20e24
Len Adler <lmadler72@eclipso.de>
2024-12-29 21:39:05 +0100

    Uebung 9-2

---
+++ src/org/hbrs/se1/ws24/exercises/uebung9/BuchungDAO.java
@@ -0,0 +1,23 @@
+package org.hbrs.se1.ws24.exercises.uebung9;
+
+import java.util.List;
+import java.util.Objects;
+
+public class BuchungDAO {
+	private List<Buchung> buchungen;
+
+	Buchung createBuchung(String reiseZiel, String datum) {
+	}
+
+	Buchung readBuchung(String id) {
+	}
+
+	void updateBuchung(Buchung b) {
+	}
+
+	void deleteBuchung(String id) {
+	}
+
+	public List<Buchung> getAllBuchungen() {
+	}
+}
